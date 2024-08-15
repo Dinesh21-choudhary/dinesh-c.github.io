4d79d980c4ec5db9dd92cbfde1d5f21dace4fa11
Dinesh21-choudhary <dinesh21chinnu@gmail.com>
2024-08-15 17:57:42 -0400

    Deploy React app with updated build

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeroBgAnimation from '../HeroBgAnimation'
-import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
+import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle, ResumeButton } from './HeroStyle'
 import HeroImg from '/Users/dinesh/portfolio/src/images/IMG_8244.jpeg'
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
